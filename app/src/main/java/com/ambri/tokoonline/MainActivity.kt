dc130a2ef5670ce06faf8ff3d0b4c4bd4991053a
sp1 <ambricam@gmail.com>
2021-07-15 14:33:21 +0700

    First init

---
+++ app/src/main/java/com/ambri/tokoonline/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ambri.tokoonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
